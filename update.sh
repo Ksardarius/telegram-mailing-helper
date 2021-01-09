53ea8f393081004b0e617e6303b4e11219a61021
cherkashin <alexander.cherkashin@sibit.tech>
2021-01-09 15:21:55 +0700

    0.1.6 update.sh fixes

--- update.sh
+++ update.sh
@@ -3,13 +3,13 @@
 workdir=`dirname $0`
 echo "Workdir: $workdir"
 sudo service telegram-mailing-helper stop
-echo "CURRENT VERSION: $(git -C $workdir/telegram-mailing-helper describe --tags --abbrev=0)"
-git -C $workdir/telegram-mailing-helper pull || true
+echo "CURRENT VERSION: $(git -C $workdir describe --tags --abbrev=0)"
+git -C $workdir pull || true
 echo "list of available version:"
-git -C $workdir/telegram-mailing-helper tag
+git -C $workdir tag
 read -p 'Please set update version: ' version
-git -C $workdir/telegram-mailing-helper checkout $version
-echo "Switch into version: $(git -C $workdir/telegram-mailing-helper describe --tags --abbrev=0)"
+git -C $workdir checkout $version
+echo "Switch into version: $(git -C $workdir describe --tags --abbrev=0)"
 echo "Start..."
 sudo service telegram-mailing-helper start
 echo "Started... please check info! on 'http://host:port/info"
